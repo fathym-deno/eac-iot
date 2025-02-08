a390a6fb4acbb66d999b7888867d7cf8cd912eba
mcgear <michael.gearhardt@gmail.com>
2025-02-08 09:14:23 -0700

    ref updates

--- src/utils/.deps.ts
+++ src/utils/.deps.ts
@@ -4,8 +4,8 @@ export type {
   EaCCloudAsCode,
   EaCCloudAzureDetails,
   EverythingAsCodeClouds,
-} from "jsr:@fathym/eac-azure@0.0.19";
-export { loadAzureCloudCredentials } from "jsr:@fathym/eac-azure@0.0.19/utils";
+} from "jsr:@fathym/eac-azure@0.0.20";
+export { loadAzureCloudCredentials } from "jsr:@fathym/eac-azure@0.0.20/utils";
 
 export { Logger } from "jsr:@std/log@0.224.9/logger";
 export { IotHubClient } from "npm:@azure/arm-iothub@6.3.0";
